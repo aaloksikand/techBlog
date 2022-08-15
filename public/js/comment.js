4356612561b616a3745c05eb0b24e82e7d1fb4cf
aaloksikand <aalok.sikand@gmail.com>
2022-08-15 16:12:33 -0700

    changed review.js to comment.js implementing suggested comments as actual changes

---
+++ public/js/comment.js
@@ -0,0 +1,32 @@
+// on click of comment button, grab specific blog post and add the relevant comments and users that made the reviews
+const textAreaEl = document.querySelector("textarea");
+const commentButtonEl = document.querySelector(".comment-button");
+
+reviewButtonEl.addEventListener("click", async ()=>{
+    const currentUrl = window.location;
+    const urlArr = currentUrl.href.split("/");
+    const blogId = urlArr[urlArr.length - 1]
+
+    try {
+              const response = await fetch('/api/comments', {
+            method: 'POST', 
+            body: JSON.stringify({ content: textAreaEl.value, blog_id: blogId, user_id: userId }),
+            headers: { 'Content-Type': 'application/json' },
+          });
+          if (response.ok) {
+            console.log(response)
+            let url = response.url.split("/")
+            if (url[url.length - 1] === "login") {
+
+              document.location.replace(`/login`);
+            } else {
+            document.location.replace(`/blog/${blogId}`);
+            }
+          } else {
+            alert('Failed to sign up.');
+          }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+});
