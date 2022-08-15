3eb9f72c10486beb071b88631b27a0fc13ff06ea
aaloksikand <aalok.sikand@gmail.com>
2022-08-15 15:19:35 -0700

    revised models/index.js based on Project 2

--- models/index.js
+++ models/index.js
@@ -1,22 +1,22 @@
 const User = require('./User');
-const Movie = require('./Movie');  //change to const Blog = require ('./Blog');
-const Review = require('./Review');  //change to const Comment = require ('./Comment');
+const Blog = require('./Blog');  //change to const Blog = require ('./Blog');
+const Comment = require('./Comment');  //change to const Comment = require ('./Comment');
 // a user can post many blogs
-User.hasMany(Review, {  //change to User.hasMany (Blog, {
+User.hasMany(Comment, {  //change to User.hasMany (Blog, {
     foreignKey: 'user_id'
 });
-// a movie can have many reviews
-Movie.hasMany(Review, {  //change to Blog.hasMany(Comment, {
-    foreignKey: 'movie_id'  //change to foreignKey: 'blog_id'
+// a blog can have many Comments
+Blog.hasMany(Comment, {  //change to Blog.hasMany(Comment, {
+    foreignKey: 'blog_id'  //change to foreignKey: 'blog_id'
 })
-// a review belongs to one movie
-Review.belongsTo(Movie, { //change to Comment.belongsTo(Blog, {
-  foreignKey: 'movie_id',  //change to 'blog_id'
+// a Comment belongs to one blog
+Comment.belongsTo(Blog, { //change to Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',  //change to 'blog_id'
 });
-// a review belongs to one user
-Review.belongsTo(User, {  //change to Comment.belongsTo(User, {
+// a Comment belongs to one user
+Comment.belongsTo(User, {  //change to Comment.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
-module.exports = { User, Movie, Review };  //change to module.exports = { User, Blog, Comment };
+module.exports = { User, Blog, Comment };  //change to module.exports = { User, Blog, Comment };
 
