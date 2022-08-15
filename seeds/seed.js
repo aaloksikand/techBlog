035a83679a40832e63440f0dc7e010299bfd5b04
aaloksikand <aalok.sikand@gmail.com>
2022-08-15 16:22:07 -0700

    changed seeds/seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,20 @@
 const sequelize = require('../config/connection');
 
-const { Movie } = require('../models');
+const { Blog } = require('../models');
 const { User } = require('../models');
-const { Review } = require('../models');
+const { Comment } = require('../models');
 
 const userData = require('./userData.json');
-const movieData = require('./movieData.json');
-const reviewData = require('./reviewData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
 
 // Use the data from seeds file and send it with the format we setup in models folder
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const movies = await Movie.bulkCreate(movieData);
+  const blogs = await Blog.bulkCreate(blogData);
   const users = await User.bulkCreate(userData);
-  const reviews = await Review.bulkCreate(reviewData);
+  const Comments = await Comment.bulkCreate(commentData);
   
 
   process.exit(0);
