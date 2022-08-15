c812bd3c50332774f2a5e2f4ac855b99b8ea516f
aaloksikand <aalok.sikand@gmail.com>
2022-08-15 14:13:23 -0700

    revised /controllers/api/index.js using Project 2 as a template

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
-//change reviewRoutes to blogRoutes
-const reviewRoutes = require('./reviewRoutes');
-//add const commentRoutes = require('./commentRoutes');
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-//change to /blogs and blogRoutes;
-router.use('/reviews', reviewRoutes);
-//add router.use ('/comments', commentRoutes);
+router.use('/blogs', blogRoutes);
+router.use ('/comments', commentRoutes);
 
 module.exports = router;
-
