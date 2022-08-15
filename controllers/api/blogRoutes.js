b1f1b38d024463e006b0d591455cba724ab90535
aaloksikand <aalok.sikand@gmail.com>
2022-08-15 14:15:38 -0700

    edited blogRoutes.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,20 +2,20 @@
 
 const router = require('express').Router();
 //change {Review} to {Blog}
-const { Review } = require('../../models');
+const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // require user to be logged in to post a review.
 router.post('/', withAuth, async (req, res) => {
   try {
     // create new review with the information passed in the body. pass the current user_id when creating a new Review object. 
-    const newReview = await Review.create({  //change to const newReview = await Review.create({
+    const newBlog = await Blog.create({  //change to const newReview = await Review.create({
       ...req.body,
       user_id: req.session.user_id,
     });
     
 
-    res.status(200).json(newReview);  //change to (newBlog)
+    res.status(200).json(newBlog);  //change to (newBlog)
   } catch (err) {
     res.status(400).json(err);
   }
